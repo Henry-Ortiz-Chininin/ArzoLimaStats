a6564bfbd905e4ecf16064b6fddcce4a650d5dbf
Henry Ortiz <desarrollador01@conadisperu.gob.pe>
2022-11-16 06:22:53 -0800

    parroquia mockup

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+
+Route::get('/parroquia', 'ParroquiaController@index')->middleware(['auth'])->name('parroquia');
+
+
 require __DIR__.'/auth.php';
 
 // -------------------------------------------------------------------------
